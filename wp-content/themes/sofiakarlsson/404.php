d2d6b0e530cbaa364360b86b87ce418b18fe6fed
Nicke Forsberg <nickeforsberg@rockheart.se>
2014-10-14 22:29:03 +0200

    SV & EN 404 page

--- wp-content/themes/sofiakarlsson/404.php
+++ wp-content/themes/sofiakarlsson/404.php
@@ -5,14 +5,28 @@
 		<div class="row blurbs">
 			<div class="col-sm-12 blurb">
 				<center>
+					
+					<?php if( ICL_LANGUAGE_CODE=='sv' ): ?>
+					
 					<h1><?php _e( '404 - Sidan kunde inte hittas.', THEME_TEXTDOMAIN ); ?></h1>
 					<h3><?php _e( 'Det verkar som vi inte kan hitta det du letar efter.', THEME_TEXTDOMAIN ); ?></h3>
 					<p>
 						<a href="<?php bloginfo('url'); ?>">
 							<span class="typcn typcn-chevron-right"><!-- --></span>
-							<?php _e( 'Tllbaka till Startsidan', THEME_TEXTDOMAIN ); ?>
+							<?php _e( 'Tillbaka till Startsidan', THEME_TEXTDOMAIN ); ?>
 						</a>
 					</p>
+					<?php else: ?>
+					<h1><?php _e( '404 - Page not found', THEME_TEXTDOMAIN ); ?></h1>
+					<h3><?php _e( 'It seems we can not find what you are looking for.', THEME_TEXTDOMAIN ); ?></h3>
+					<p>
+						<a href="<?php bloginfo('url'); ?>">
+							<span class="typcn typcn-chevron-right"><!-- --></span>
+							<?php _e( 'Back to Start', THEME_TEXTDOMAIN ); ?>
+						</a>
+					</p>
+					<?php endif; ?>
+					
 				</center>
 			</div>
 		</div>
