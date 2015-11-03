d444b28e9528f9d666d44c6565ccbdece3c066d1
Nicke Forsberg <nickeforsberg@rockheart.se>
2015-11-03 16:14:10 +0100

    adding latest logo and options

--- wp-content/themes/sofiakarlsson/footer.php
+++ wp-content/themes/sofiakarlsson/footer.php
@@ -1,11 +1,19 @@
+	<?php if( is_home() || is_front_page() || is_page_template('template-startpage.php') ) : ?>
+		<?php
+		$jumbotron_image = get_field('jumbotron_image', 'options');
+		?>
+		<?php if( $jumbotron_image ) : ?>
+		<div class="jumbotron">
+			<figure class="background-image" style="background-image:url(<?php echo $jumbotron_image['url']; ?>);"></figure>
+		</div>
+		<?php endif; ?>
+	<?php endif; ?>
 	<footer id="footer">
 		<div class="container">
 			<div class="row blurbs">
-
 				<div class="col-sm-12 blurb">
 					<p><?php _e('Copyright &copy;', THEME_TEXTDOMAIN);?> <?php echo date("Y"); ?> <?php bloginfo('name'); ?>. <?php _e('All Rights Reserved.', THEME_TEXTDOMAIN); ?></p>
 				</div>
-
 			</div>
 		</div>
 	</footer>
--- wp-content/themes/sofiakarlsson/template-startpage.php
+++ wp-content/themes/sofiakarlsson/template-startpage.php
@@ -10,7 +10,7 @@ get_header();
 <section id="cover-area">
 	<div class="table">
 		<div class="table-cell">
-			<img class="no-ie" src="<?php echo get_template_directory_uri();?>/ui/images/SK_logo_white.svg" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
+			<img class="no-ie" src="<?php echo get_template_directory_uri();?>/ui/images/SK_logo_new.svg" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
 			 <a href="#content" class="scroll-to fa fa-chevron-down"></a>
 		</div>
 	</div>
