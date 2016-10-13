1db33c2ec6da9304109608e5785ec852662b2d01
Nicke Forsberg <niklas.forsberg@triggerfish.se>
2016-10-13 09:45:52 +0200

    Removing old facebook graph api

--- wp-content/themes/sofiakarlsson/header.php
+++ wp-content/themes/sofiakarlsson/header.php
@@ -52,15 +52,6 @@
 </head>
 <body <?php body_class(); ?>>
 
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/sv_SE/sdk.js#xfbml=1&appId=1516541798588138&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
 <?php
 $header_scripts = get_field('header_scripts', 'options');
 
